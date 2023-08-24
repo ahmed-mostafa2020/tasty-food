628e05f74a6f32bd8b3b667966f332aa473ccf5c
Ahmed Mostafa <ahmed-mostafa2020>
2023-08-24 03:39:48 +0300

    lang done but local storage left1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,13 @@ import i18n from "../i18n";
 const Navbar = () => {
   const { myMode, setMyMode } = ChangeTheme();
 
-  const handleClick = () => {
+  const handleLang = () => {
+    i18n.language == "en"
+      ? i18n.changeLanguage("ar")
+      : i18n.changeLanguage("en");
+  };
+
+  const handleTheme = () => {
     localStorage.setItem("currentMode", myMode === "light" ? "dark" : "light");
     setMyMode(myMode === "light" ? "dark" : "light");
   };
@@ -14,9 +20,9 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <h1>Hii, i am navbar</h1>
-      {/* Lang */}
 
-      {i18n.language == "en" && (
+      {/* Lang */}
+      {/* {i18n.language == "en" && (
         <Button
           onClick={() => {
             i18n.changeLanguage("ar");
@@ -34,11 +40,16 @@ const Navbar = () => {
         >
           en
         </Button>
+      )} */}
+
+      {i18n.language === "en" ? (
+        <Button onClick={handleLang}>ar</Button>
+      ) : (
+        <Button onClick={handleLang}>en</Button>
       )}
 
       {/* Theme */}
-
-      <Button sx={{ ml: 1 }} onClick={handleClick} color="inherit">
+      <Button sx={{ ml: 1 }} onClick={handleTheme} color="inherit">
         {myMode === "dark" ? (
           <Brightness7 sx={{ color: "orange" }} />
         ) : (
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -19,7 +19,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources, // resources are important to load translations for the languages.
-    lng: "ar", // It acts as default language. When the site loads, content is shown in this language.
+    lng: "en", // It acts as default language. When the site loads, content is shown in this language.
     debug: true,
     fallbackLng: "ar", // use de if selected language is not available
     interpolation: {
