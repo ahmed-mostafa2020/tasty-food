8229b8a462249d699c924db40c3e219d7ae06af0
Ahmed Mostafa <ahmed-mostafa2020>
2024-01-12 11:20:25 +0200

    items almost done

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,19 +1,37 @@
 import { Container } from "@mui/material";
 import { ChangeTheme } from "../context/ThemeContext";
 import { FetchingAllEndPointsData } from "../context/FetchingDataContext";
-import DownShapedBorder from "../atomicDesign/atoms/DownShapedBorder";
+import Image from "next/image";
 
 const Items = () => {
   const { myPalette } = ChangeTheme();
-
   const { homeEndPointData } = FetchingAllEndPointsData();
+  const allItems = homeEndPointData && homeEndPointData.data.category;
+
   return (
     <section
       className="items"
       style={{ backgroundColor: myPalette.background.shaped_border }}
     >
       <Container fixed className="container">
-        <p className="rights">{homeEndPointData.data.brand.copy_rights}</p>
+        {allItems.map((item) => (
+          <div className="item" key={item.name}>
+            <div className="item-box">
+              <figure>
+                <Image
+                  className="item-image"
+                  loader={() => `${item.image}`}
+                  src={`${item.image}`}
+                  alt="item"
+                  width={110}
+                  height={75}
+                />
+              </figure>
+
+              <h4 className="item-name">{item.name}</h4>
+            </div>
+          </div>
+        ))}
       </Container>
     </section>
   );
