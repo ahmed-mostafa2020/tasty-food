79070d8b9e334ac04622e94fb6f859997e50f22f
Ahmed Mostafa <ahmed-mostafa2020>
2024-01-16 15:14:43 +0200

    w o product

--- src/components/FilterGallery.js
+++ src/components/FilterGallery.js
@@ -1,3 +1,4 @@
+import { Container } from "@mui/material";
 import SectionTitle from "../atomicDesign/atoms/SectionTitle";
 import FilterButtons from "../atomicDesign/molecules/FilterButtons";
 import Product from "../components/Product";
@@ -5,18 +6,26 @@ import Product from "../components/Product";
 const FilterGallery = () => {
   return (
     <section className="filterGallery">
-      <SectionTitle title={"Our Popular Tasty Foods"} fontSize={"35px"} />
+      <Container>
+        <SectionTitle title={"Our Popular Tasty Foods"} fontSize={"35px"} />
 
-      <article>
-        Enim ut et amet vitae facilisi vel odio nisl. Pellentesque malesuada
-        massa proin cursus elit amet iaculis.
-      </article>
+        <article>
+          Enim ut et amet vitae facilisi vel odio nisl. Pellentesque malesuada
+          massa proin cursus elit amet iaculis.
+        </article>
 
-      <FilterButtons />
+        <FilterButtons />
 
-      <div className="productsBox">
-        <Product />
-      </div>
+        <div className="productsBox">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </Container>
     </section>
   );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import product from "../../public/assets/images/B-2 3.png";
+
 const Product = () => {
-  return <div className="product">Product</div>;
+  return (
+    <div className="product">
+      <Image src={product} alt="product" width={200} height={150} />
+
+      <h4>Spicy Burger</h4>
+      <p>Get a 20% Discount On First Order</p>
+      <h5>$10.5</h5>
+    </div>
+  );
 };
 
 export default Product;
