b8cf652fba1385fcd4f1f7dbf9cf651d0d8525b4
Ahmed Mostafa <ahmed-mostafa2020>
2024-01-21 15:19:24 +0200

    w o menu

--- pages/menu.js
+++ pages/menu.js
@@ -5,9 +5,48 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Container } from "@mui/material";
 import LayoutTopWrapper from "../src/atomicDesign/organisms/LayoutTopWrapper";
+import MenuItem from "../src/components/MenuItem";
+import SectionTitle from "../src/atomicDesign/atoms/SectionTitle";
 
 const Menu = ({ data }) => {
   const { t } = useTranslation();
+
+  const allMenuItems = [
+    {
+      name: "Burger",
+      subItems: [
+        {
+          name: "Cheese Burger",
+          price: "30EGP",
+          description:
+            "In justo maecenas viverra pulvinar phasellus mattis ac mi.",
+        },
+        {
+          name: "Crispy veg cheese Burger",
+          price: "33EGP",
+          description:
+            "In justo maecenas viverra pulvinar phasellus mattis ac mi.",
+        },
+      ],
+    },
+    {
+      name: "Pizza",
+      subItems: [
+        {
+          name: "Margherita Pizza",
+          price: "35EGP",
+          description:
+            "In justo maecenas viverra pulvinar phasellus mattis ac mi.",
+        },
+        {
+          name: "Garden Delight Pizza",
+          price: "31EGP",
+          description:
+            "In justo maecenas viverra pulvinar phasellus mattis ac mi.",
+        },
+      ],
+    },
+  ];
 
   return (
     <>
@@ -23,9 +62,27 @@ const Menu = ({ data }) => {
 
       <Layout>
         <LayoutTopWrapper title={t("Menu")} />
-        <main className="menu" style={{ textAlign: "center" }}>
+        <main className="menu">
           <Container fixed className="container">
-            Menu
+            {allMenuItems.map((menuItem, index) => (
+              <section key={index} className="menuItem">
+                <SectionTitle title={menuItem.name} fontSize={"40px"} />
+
+                <div className="subItemsContainer">
+                  {menuItem.subItems.map((subItem, index) => (
+                    <div key={index} className="subItem">
+                      <div className="subItemBox">
+                        <h4>{subItem.name}</h4>
+                        <span>....</span>
+                        <h5>{subItem.price}</h5>
+                      </div>
+
+                      <p>{subItem.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            ))}
           </Container>
         </main>
       </Layout>
